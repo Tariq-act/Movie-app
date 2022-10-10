52537c8ff9f507c0bd5a2b18a310d4ce459e0663
Tariq Khan <Khantarikact@gmail.com>
2022-10-11 01:13:23 +0530

    implement jsx if array was empty

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -9,15 +9,30 @@ import './MovieList.styles.css';
 const MovieList = () => {
   const { movieList } = useContext(MovieContext);
   return (
-    <div className='movie-list'>
-      {movieList ? (
-        movieList.map((movie) => (
-          <MovieCard key={movie.id} movie={movie} href={`/movie/${movie.id}`} />
-        ))
-      ) : (
-        <h1>No Result Found</h1>
+    <>
+      <div className='movie-list'>
+        {movieList.length > 0 &&
+          movieList.map((movie) => (
+            <MovieCard
+              key={movie.id}
+              movie={movie}
+              href={`/movie/${movie.id}`}
+            />
+          ))}
+      </div>
+
+      {movieList.length === 0 && (
+        <h1
+          style={{
+            color: 'white',
+            textAlign: 'center',
+            marginTop: '2rem',
+          }}
+        >
+          No Result Found
+        </h1>
       )}
-    </div>
+    </>
   );
 };
 
